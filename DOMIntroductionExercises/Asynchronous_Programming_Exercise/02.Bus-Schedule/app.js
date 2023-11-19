9c4c0413da6c683a9891d83a8b73359cbdd3b965
Aleksandar <alexandernovoselski@yahoo.com>
2023-11-19 17:54:22 +0200

    Updating README, adding logic to Bus-Schedule

--- DOMIntroductionExercises/Asynchronous_Programming_Exercise/02.Bus-Schedule/app.js
+++ DOMIntroductionExercises/Asynchronous_Programming_Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,31 @@
 function solve() {
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let info = document.querySelector('div#info span.info');
+    let nextStopId = 'depot';
+    let stopName = '';
+    async function depart() {
+        try {
+            let url = `http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`
+            let response = await fetch(url);
+            let data = await response.json();
+            stopName = data.name;
+            nextStopId = data.next;
 
-    function depart() {
-        console.log('Depart TODO...');
+            info.innerText = `Next stop ${stopName}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+
+        } catch (error) {
+            info.innerText = "Error";
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.innerText = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This repository is simply for exercising DOM Manipulation, functions and asynchronously programming, featuring some unit testing with mocha and chai.
+
+
+To build the server API, run the server with node server.js then you can start sending requests to the local server :/
+
+CRUD Operations
+All requests are sent to /jsonstore/:resource. Resources can be nested and have any shape. Individual properties can be accessed by appending /:propName to the endpoint as deep as you require. Supported requests are GET, POST, PUT, PATCH, DELETE
